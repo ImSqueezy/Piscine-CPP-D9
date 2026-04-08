fe88014ce2418512f92d36dd38970f1c483f77df
ImSqueezy <ouaallaabdelali1@gmail.com>
2026-04-08 18:44:32 +0100

    Updated main.cpp

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,45 +1,51 @@
 #include "BitcoinExchange.hpp"
 
+void	ft_validate_date(std::vector<Data> data) {
+    std::vector<Data>::iterator it;
+    for (it = data.begin(); it != data.end(); ++it) {
+        Data& element = *it;
+
+    }
+}
+
 int main(int ac, char **av) {
     if (ac != 2) {
         std::cout << "Error: The program takes two arguments!" << std::endl;
         return (1);
     }
 
-    std::fstream        file(av[1]);
-    std::string         line;
-    std::vector<Data>   data;
+	std::ifstream		iFile(av[1]);
+	if (!iFile.is_open()) {
+		std::cerr << "Error opening the file!" << std::endl;
+		return 1;
+	}
+	std::string			line;
+	std::vector<Data>	data;
 
-    std::getline(file, line);
-    for (line; std::getline(file, line);) {
-        std::istringstream  iss(line);
-        std::string         word;
-        Data                new_data;
-        std::cout << "line: " << line << std::endl;
+    for (std::string line; std::getline(iFile, line);) {
+		if (line.empty() || line == "date | value")
+			continue;
 
-        std::getline(iss, word, '-');
-        std::istringstream(word) >> new_data.year;
-        
-        std::getline(iss, word, '-');
-        std::istringstream(word) >> new_data.month;
-
-        std::getline(iss, word, '-');
-        std::istringstream(word) >> new_data.day;
-
-        std::getline(iss, word);
-        std::istringstream(word) >> new_data.btc;
+		std::istringstream	iss(line);
+		Data				new_data;
+		char				dash0, dash1, pipe;
+		std::cout << line << std::endl;
+		
+		iss >> new_data.year >> dash0 >> new_data.month >> dash1 >> new_data.day;
+		if (dash0 != '-' || dash1 != '-')
+			new_data.valid_input = false;
+		if (iss >> pipe && pipe == '|') {
+			if (!(iss >> new_data.btc)) {
+				new_data.valid_btc = false;
+			} else
+				new_data.valid_btc = true;
+		} else {
+			new_data.valid_input = false;
+		}
 
         data.push_back(new_data);
     }
-
-    std::vector<Data>::iterator it;
-
-    for (it = data.begin(); it != data.end(); ++it) {
-        Data& element = *it;
-        std::cout << "year " << element.year << std::endl;
-        std::cout << "month " << element.month << std::endl;
-        std::cout << "day " << element.day << std::endl;
-    }
-    
+	iFile.close();
+	ft_validate_date(data);
     return 0;
 }
