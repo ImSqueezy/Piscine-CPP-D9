bc1d2c8c3402f35160a16a8fec19272dc8b10d29
ImSqueezy <ouaallaabdelali1@gmail.com>
2026-04-15 11:10:56 +0100

    Addition to PmergeMe, parseInput() added and sort() has some basic tests

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -1,21 +1,43 @@
 #include "PmergeMe.hpp"
 
-PmergeMe::PmergeMe()
-{
-}
+PmergeMe::PmergeMe() {}
 
-PmergeMe::PmergeMe(const PmergeMe &src)
-{
+PmergeMe::PmergeMe(const PmergeMe &src) {
     *this = src;
 }
 
-PmergeMe &PmergeMe::operator=(const PmergeMe &src)
-{
-    if (this != &src)
-	   // to copy
+PmergeMe &PmergeMe::operator=(const PmergeMe &src) {
+    if (this != &src) {
+        this->_vec = src._vec;
+        this->_deq = src._deq;
+    }
     return *this;
 }
 
-PmergeMe::~PmergeMe()
-{
+PmergeMe::~PmergeMe() {}
+
+void	PmergeMe::parseInput(int ac, char** av) {
+	for (int i = 1; i < ac; ++i) {
+		std::istringstream	iss(av[i]);
+		int					n;
+		if (!(iss >> n) || n < 0)
+			throw std::runtime_error("Error: invalid input");
+		std::string leftover;
+		if (iss >> leftover)
+			throw std::runtime_error("Error: invalid input");
+		_vec.push_back(n);
+		_deq.push_back(n);
+	}
+}
+
+void	PmergeMe::sort() {
+	// view what's in _vec and _deq
+	std::cout << "_deq has: " << std::endl;
+	for (std::deque<int>::iterator it = _deq.begin(); it != _deq.end(); ++it) {
+		std::cout << *it << std::endl;
+	}
+	std::cout << "_vec has: " << std::endl;
+	for (std::vector<int>::iterator it = _vec.begin(); it != _vec.end(); ++it) {
+		std::cout << *it << std::endl;
+	}
 }
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -2,14 +2,25 @@
 # define PMERGEME_HPP
 
 # include <iostream>
+# include <vector>
+# include <deque>
+# include <sstream>
+# include <stdexcept>
 
 class PmergeMe
 {
+	private:
+		std::vector<int>	_vec;
+		std::deque<int>		_deq;
+
     public:
-        PmergeMe();
-        PmergeMe(const PmergeMe &src);
-        PmergeMe &operator=(const PmergeMe &src);
-        ~PmergeMe();
+    	PmergeMe();
+    	PmergeMe(const PmergeMe &src);
+    	PmergeMe &operator=(const PmergeMe &src);
+    	~PmergeMe();
+
+		void	parseInput(int ac, char** av);
+		void	sort();
 };
 
 #endif
