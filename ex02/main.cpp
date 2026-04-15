953e8365b8ed7a4952d74320696caec6f9a77f6f
ImSqueezy <ouaallaabdelali1@gmail.com>
2026-04-15 11:00:40 +0100

    main.cpp to have the main idea (having parseInput() that takes ac and av and sort() to sort after validation of the input)

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,11 +1,19 @@
 #include "PmergeMe.hpp"
 
-int main(int argc, char **argv)
-{
-    (void)argc;
-    (void)argv;
-
-    PmergeMe pmerge;
-
-    return 0;
+int	main(int ac, char** av) {
+	if (ac < 2) {
+		std::cerr << "Error: Usage: ./PmergeMe <int1> <int2> ..." << std::endl;
+		return 1;
+	}
+	// try catch because i will throw errors when an invalid input is given or else
+	try {
+		PmergeMe sorter;
+		sorter.parseInput(ac, av);
+		// i will later have this sort the numbers, i don't know how yet
+		sorter.sort();
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
 }
