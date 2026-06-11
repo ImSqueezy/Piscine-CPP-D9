16ed140ecb3c2bac45fa66b754e990ddcd901360
ImSqueezy <ouaallaabdelali1@gmail.com>
2026-06-11 18:29:25 +0100

    building the main chain

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -20,19 +20,16 @@ class PmergeMe {
 		template<typename Container>
 		void	mergeInsertionSort(Container& seq) {
 			int		n = seq.size();
-
-			if (n <= 1)
-				return ;
 			int		strag = 0;
 			bool	odd = n % 2;
 
-			(void)strag;
+			if (n <= 1)
+				return ;
 			if (odd) {
 				strag = seq.back();
 				seq.pop_back();
 				n--;
 			}
-
 			std::vector<std::pair<int, int> > pairs;
 			for (int i = 0; i < n; i += 2) {
 				int first = seq[i];
@@ -42,14 +39,31 @@ class PmergeMe {
 					std::swap(first, second);
 				pairs.push_back(std::make_pair(first, second));
 			}
-
-			// Example: pairs [(3,1), (5,2)] → largers [3, 5]
-			Container largers;
-			std::cout << "size here is: " << pairs.size() << std::endl;
+			Container			largers;
 			for (size_t i = 0; i < pairs.size(); ++i)
 				largers.push_back(pairs[i].first);
-
 			mergeInsertionSort(largers);
+			std::vector<std::pair<int, int> > sortedPairs;
+			for (size_t i = 0; i < largers.size(); ++i) {
+				// looking for the pair containing this larger element
+				for (size_t j = 0; j < pairs.size(); ++j) {
+					if (pairs[j].first == largers[i]) {
+						sortedPairs.push_back(pairs[j]);
+						pairs.erase(pairs.begin() + j);  // might remove to avoid dups 
+						break;
+					}
+				}
+			}
+			Container			chain;
+			chain.push_back(sortedPairs[0].second);
+			for (size_t i = 0; i < sortedPairs.size(); ++i)
+				chain.push_back(sortedPairs[i].first);
+			std::vector<int>	pend;
+			for (size_t i = 1; i < sortedPairs.size(); ++i)
+				pend.push_back(sortedPairs[i].second);
+			std::vector<int>	pPos(pend.size());
+			for (size_t i = 0; i < pend.size(); ++i)
+				pPos[i] = i + 2;
 
 		}
 
