5c3a37171d62f5ac3c3d11e46e932e3970b8f343
ImSqueezy <ouaallaabdelali1@gmail.com>
2026-06-11 15:43:42 +0100

    trying out templates

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -12,11 +12,46 @@
 
 class PmergeMe {
 	private:
-		std::vector<int> _vec;
-		std::deque<int> _deq;
+		std::vector<int>	_vec;
+		std::deque<int>		_deq;
 
-		void mergeInsertionSortVec(std::vector<int>& seq);
-		void mergeInsertionSortDeq(std::deque<int>& seq);
+		static std::vector<int>	generateJacobsthalOrder(int n);
+
+		template<typename Container>
+		void	mergeInsertionSort(Container& seq) {
+			int		n = seq.size();
+
+			if (n <= 1)
+				return 0;
+			int		strag = 0;
+			bool	odd = n % 2;
+
+			(void)strag;
+			if (odd) {
+				strag = seq.back();
+				seq.pop_back();
+				n--;
+			}
+
+			std::vector<std::pair<int, int> > pairs;
+			for (int i = 0; i < n; i += 2) {
+				int first = seq[i];
+				int second = seq[i+1];
+
+				if (first < second)
+					std::swap(first, second);
+				pairs.push_back(std::make_pair(first, second));
+			}
+			std::vector<std::pair<int, int> >::iterator it;
+
+			for (it = pairs.begin(); it != pairs.end(); ++it) {
+				std::cout << "first: " << it->first
+						  << ", second: " << it->second
+						  << std::endl;
+			}
+
+		}
+
 	public:
 		PmergeMe();
 		PmergeMe(const PmergeMe &src);
